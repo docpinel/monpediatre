14b7b1591bca0ae20af1749428cc0b61a3576a88
docpinel <mhiri.kais@live.fr>
2025-01-03 07:22:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config(object):
-    DEBUG = True  # Changez à False pour la production
+    DEBUG = False  # Changez à False pour la production
     SECRET_KEY = '94XtlzU0yCpL4y0hg6VBhPf59xhyhPFz'
